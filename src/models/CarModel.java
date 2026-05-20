ffb0ea8d1fe71a3b95d8449a084ec51d38137ed9
Your Name <you@example.com>
2026-05-20 13:57:09 +0000

    frits

--- src/models/CarModel.java
+++ src/models/CarModel.java
@@ -1,5 +1,37 @@
 package models;
 
+import java.util.Arrays;
+
 public class CarModel {
+  private String modelName;
+  private CarYear[] years;
+  
+  public CarModel(String modelName, CarYear[] years) {
+    this.modelName = modelName;
+    this.years = years;
+  }
+
+  public String getModelName() {
+    return modelName;
+  }
+
+  public void setModelName(String modelName) {
+    this.modelName = modelName;
+  }
+
+  public CarYear[] getYears() {
+    return years;
+  }
+
+  public void setYears(CarYear[] years) {
+    this.years = years;
+  }
+
+  @Override
+  public String toString() {
+    return "CarModel [modelName=" + modelName + ", years=" + Arrays.toString(years) + "]";
+  }
+  
+
 
 }
